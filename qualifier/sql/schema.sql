ffad44032a216efee486ca655d10a4c396924f70
tyage <namatyage@gmail.com>
2015-09-25 17:01:15 +0900

    added index

--- qualifier/sql/schema.sql
+++ qualifier/sql/schema.sql
@@ -11,5 +11,10 @@ CREATE TABLE IF NOT EXISTS `login_log` (
   `user_id` int,
   `login` varchar(255) NOT NULL,
   `ip` varchar(255) NOT NULL,
-  `succeeded` tinyint NOT NULL
+  `succeeded` tinyint NOT NULL,
+
+  KEY (user_id),
+  KEY (ip),
+  KEY (succeeded, user_id),
+  KEY (succeeded, ip)
 ) DEFAULT CHARSET=utf8;
