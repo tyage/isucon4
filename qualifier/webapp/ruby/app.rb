8647759c510a506d6f322fbb2136237dd36f064e
tyage <namatyage@gmail.com>
2015-09-25 17:08:30 +0900

    dont use password hash

--- qualifier/webapp/ruby/app.rb
+++ qualifier/webapp/ruby/app.rb
@@ -31,7 +31,7 @@ module Isucon4
       end
 
       def calculate_password_hash(password, salt)
-        Digest::SHA256.hexdigest "#{password}:#{salt}"
+        password
       end
 
       def login_log(succeeded, login, user_id = nil)
